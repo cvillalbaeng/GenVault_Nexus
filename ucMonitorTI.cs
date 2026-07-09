15389467c4732a071158cb17fe43a9c0a7f5364a
aburrido <235208023+aburridokarate-collab@users.noreply.github.com>
2026-07-08 21:57:30 -0400

    Finalizado el Módulo 4: Monitor de Infraestructura TI con simulación Zabbix y optimización

---
+++ ucMonitorTI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonitorTI
+{
+    public partial class ucMonitorTI : UserControl
+    {
+        Random generadorAleatorio = new Random();
+
+        public ucMonitorTI()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrRed_Tick(object sender, EventArgs e)
+        {
+            // Genera un número de tráfico entre 40% y 100%
+            int traficoActual = generadorAleatorio.Next(40, 101);
+
+            pbRed.Value = traficoActual;
+            lblTrafico.Text = "Tráfico de Red Borde: " + traficoActual.ToString() + "%";
+
+            if (traficoActual >= 95)
+            {
+                lblAlertaZabbix.Visible = true;
+            }
+            else
+            {
+                lblAlertaZabbix.Visible = false;
+            }
+        }
+    }
+}
