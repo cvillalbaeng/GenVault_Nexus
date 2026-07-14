37635395a1fc6459a9b0e38dba580c379cd034a2
Rasheed <rrodriguez.9784@unimar.edu.ve>
2026-07-13 21:21:51 -0400

    feat: Modulo 3 - CRUD especimenes y simulador PAM

---
+++ ucBioinformatica.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace GenVault_Nexus
+{
+    partial class ucBioinformatica
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblEspecie = new System.Windows.Forms.Label();
+            this.txtEspecie = new System.Windows.Forms.TextBox();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.dgvEspecimenes = new System.Windows.Forms.DataGridView();
+            this.btnEjecutarPAM = new System.Windows.Forms.Button();
+            this.pbPAM = new System.Windows.Forms.ProgressBar();
+            this.lblResultadoPAM = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEspecimenes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.ForeColor = System.Drawing.Color.White;
+            this.lblCodigo.Location = new System.Drawing.Point(20, 20);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(54, 16);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(60)))));
+            this.txtCodigo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCodigo.ForeColor = System.Drawing.Color.White;
+            this.txtCodigo.Location = new System.Drawing.Point(120, 20);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 15);
+            this.txtCodigo.TabIndex = 1;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.ForeColor = System.Drawing.Color.White;
+            this.lblNombre.Location = new System.Drawing.Point(20, 55);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(130, 16);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre Espécimen:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(60)))));
+            this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNombre.ForeColor = System.Drawing.Color.White;
+            this.txtNombre.Location = new System.Drawing.Point(160, 55);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 15);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // lblEspecie
+            // 
+            this.lblEspecie.AutoSize = true;
+            this.lblEspecie.ForeColor = System.Drawing.Color.White;
+            this.lblEspecie.Location = new System.Drawing.Point(20, 90);
+            this.lblEspecie.Name = "lblEspecie";
+            this.lblEspecie.Size = new System.Drawing.Size(60, 16);
+            this.lblEspecie.TabIndex = 4;
+            this.lblEspecie.Text = "Especie:";
+            // 
+            // txtEspecie
+            // 
+            this.txtEspecie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(60)))));
+            this.txtEspecie.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEspecie.ForeColor = System.Drawing.Color.White;
+            this.txtEspecie.Location = new System.Drawing.Point(120, 90);
+            this.txtEspecie.Name = "txtEspecie";
+            this.txtEspecie.Size = new System.Drawing.Size(100, 15);
+            this.txtEspecie.TabIndex = 5;
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.ForeColor = System.Drawing.Color.White;
+            this.lblEstado.Location = new System.Drawing.Point(350, 20);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(53, 16);
+            this.lblEstado.TabIndex = 6;
+            this.lblEstado.Text = "Estado:";
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(60)))));
+            this.cmbEstado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbEstado.ForeColor = System.Drawing.Color.White;
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Items.AddRange(new object[] {
+            "En análisis",
+            "",
+            "Estable",
+            "",
+            "Mutación detectada"});
+            this.cmbEstado.Location = new System.Drawing.Point(410, 17);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(121, 24);
+            this.cmbEstado.TabIndex = 7;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.ForeColor = System.Drawing.Color.White;
+            this.lblFecha.Location = new System.Drawing.Point(350, 55);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(102, 16);
+            this.lblFecha.TabIndex = 8;
+            this.lblFecha.Text = "Fecha Registro:";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.CalendarForeColor = System.Drawing.Color.White;
+            this.dtpFecha.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(60)))));
+            this.dtpFecha.Location = new System.Drawing.Point(455, 52);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(249, 22);
+            this.dtpFecha.TabIndex = 9;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(191)))), ((int)(((byte)(165)))));
+            this.btnAgregar.FlatAppearance.BorderSize = 0;
+            this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgregar.ForeColor = System.Drawing.Color.White;
+            this.btnAgregar.Location = new System.Drawing.Point(20, 130);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(191)))), ((int)(((byte)(165)))));
+            this.btnModificar.FlatAppearance.BorderSize = 0;
+            this.btnModificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnModificar.ForeColor = System.Drawing.Color.White;
+            this.btnModificar.Location = new System.Drawing.Point(120, 130);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 11;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(191)))), ((int)(((byte)(165)))));
+            this.btnEliminar.FlatAppearance.BorderSize = 0;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(220, 130);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 12;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(191)))), ((int)(((byte)(165)))));
+            this.btnLimpiar.FlatAppearance.BorderSize = 0;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpiar.Location = new System.Drawing.Point(320, 130);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 13;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            // 
+            // dgvEspecimenes
+            // 
+            this.dgvEspecimenes.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(42)))), ((int)(((byte)(51)))));
+            this.dgvEspecimenes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEspecimenes.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(49)))), ((int)(((byte)(60)))));
+            this.dgvEspecimenes.Location = new System.Drawing.Point(20, 170);
+            this.dgvEspecimenes.Name = "dgvEspecimenes";
+            this.dgvEspecimenes.RowHeadersWidth = 51;
+            this.dgvEspecimenes.RowTemplate.Height = 24;
+            this.dgvEspecimenes.Size = new System.Drawing.Size(700, 280);
+            this.dgvEspecimenes.TabIndex = 14;
+            // 
+            // btnEjecutarPAM
+            // 
+            this.btnEjecutarPAM.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(191)))), ((int)(((byte)(165)))));
+            this.btnEjecutarPAM.FlatAppearance.BorderSize = 0;
+            this.btnEjecutarPAM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEjecutarPAM.ForeColor = System.Drawing.Color.White;
+            this.btnEjecutarPAM.Location = new System.Drawing.Point(20, 480);
+            this.btnEjecutarPAM.Name = "btnEjecutarPAM";
+            this.btnEjecutarPAM.Size = new System.Drawing.Size(100, 23);
+            this.btnEjecutarPAM.TabIndex = 15;
+            this.btnEjecutarPAM.Text = "Ejecutar PAM";
+            this.btnEjecutarPAM.UseVisualStyleBackColor = false;
+            // 
+            // pbPAM
+            // 
+            this.pbPAM.Location = new System.Drawing.Point(150, 484);
+            this.pbPAM.Name = "pbPAM";
+            this.pbPAM.Size = new System.Drawing.Size(300, 25);
+            this.pbPAM.TabIndex = 16;
+            // 
+            // lblResultadoPAM
+            // 
+            this.lblResultadoPAM.AutoSize = true;
+            this.lblResultadoPAM.ForeColor = System.Drawing.Color.White;
+            this.lblResultadoPAM.Location = new System.Drawing.Point(20, 520);
+            this.lblResultadoPAM.Name = "lblResultadoPAM";
+            this.lblResultadoPAM.Size = new System.Drawing.Size(87, 16);
+            this.lblResultadoPAM.TabIndex = 17;
+            this.lblResultadoPAM.Text = "Resultado: ---";
+            // 
+            // ucBioinformatica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(31)))), ((int)(((byte)(38)))));
+            this.Controls.Add(this.lblResultadoPAM);
+            this.Controls.Add(this.pbPAM);
+            this.Controls.Add(this.btnEjecutarPAM);
+            this.Controls.Add(this.dgvEspecimenes);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.cmbEstado);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.txtEspecie);
+            this.Controls.Add(this.lblEspecie);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblCodigo);
+            this.Name = "ucBioinformatica";
+            this.Size = new System.Drawing.Size(750, 600);
+            this.Load += new System.EventHandler(this.ucBioinformatica_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEspecimenes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblEspecie;
+        private System.Windows.Forms.TextBox txtEspecie;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridView dgvEspecimenes;
+        private System.Windows.Forms.Button btnEjecutarPAM;
+        private System.Windows.Forms.ProgressBar pbPAM;
+        private System.Windows.Forms.Label lblResultadoPAM;
+    }
+}
---
+++ ucBioinformatica.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace GenVault_Nexus
+{
+    public partial class ucBioinformatica : UserControl
+    {
+        private DataTable tablaEspecimenes;
+        private Random random = new Random();
+
+        public ucBioinformatica()
+        {
+            InitializeComponent();
+            this.Load += ucBioinformatica_Load;
+        }
+
+        private void ucBioinformatica_Load(object sender, EventArgs e)
+        {
+            InicializarTabla();
+            dgvEspecimenes.DataSource = tablaEspecimenes;
+            dgvEspecimenes.SelectionChanged += dgvEspecimenes_SelectionChanged;
+
+            btnAgregar.Click += btnAgregar_Click;
+            btnModificar.Click += btnModificar_Click;
+            btnEliminar.Click += btnEliminar_Click;
+            btnLimpiar.Click += btnLimpiar_Click;
+            btnEjecutarPAM.Click += btnEjecutarPAM_Click;
+        }
+
+        private void InicializarTabla()
+        {
+            tablaEspecimenes = new DataTable();
+            tablaEspecimenes.Columns.Add("Codigo", typeof(string));
+            tablaEspecimenes.Columns.Add("Nombre", typeof(string));
+            tablaEspecimenes.Columns.Add("Especie", typeof(string));
+            tablaEspecimenes.Columns.Add("Estado", typeof(string));
+            tablaEspecimenes.Columns.Add("Fecha", typeof(string));
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCodigo.Text) || string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show("El código y el nombre son obligatorios.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            tablaEspecimenes.Rows.Add(
+                txtCodigo.Text,
+                txtNombre.Text,
+                txtEspecie.Text,
+                cmbEstado.Text,
+                dtpFecha.Value.ToShortDateString()
+            );
+
+            LimpiarCampos();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            if (dgvEspecimenes.CurrentRow == null)
+            {
+                MessageBox.Show("Selecciona una fila de la tabla para modificar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DataRowView fila = (DataRowView)dgvEspecimenes.CurrentRow.DataBoundItem;
+            fila["Codigo"] = txtCodigo.Text;
+            fila["Nombre"] = txtNombre.Text;
+            fila["Especie"] = txtEspecie.Text;
+            fila["Estado"] = cmbEstado.Text;
+            fila["Fecha"] = dtpFecha.Value.ToShortDateString();
+
+            LimpiarCampos();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvEspecimenes.CurrentRow == null)
+            {
+                MessageBox.Show("Selecciona una fila de la tabla para eliminar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult confirmacion = MessageBox.Show("¿Seguro que deseas eliminar este espécimen?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirmacion == DialogResult.Yes)
+            {
+                DataRowView fila = (DataRowView)dgvEspecimenes.CurrentRow.DataBoundItem;
+                fila.Row.Delete();
+                LimpiarCampos();
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+        }
+
+        private void LimpiarCampos()
+        {
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtEspecie.Clear();
+            cmbEstado.SelectedIndex = -1;
+            dtpFecha.Value = DateTime.Now;
+            dgvEspecimenes.ClearSelection();
+        }
+
+        private void dgvEspecimenes_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvEspecimenes.CurrentRow == null) return;
+
+            DataRowView fila = (DataRowView)dgvEspecimenes.CurrentRow.DataBoundItem;
+            if (fila == null) return;
+
+            txtCodigo.Text = fila["Codigo"].ToString();
+            txtNombre.Text = fila["Nombre"].ToString();
+            txtEspecie.Text = fila["Especie"].ToString();
+            cmbEstado.Text = fila["Estado"].ToString();
+        }
+
+        private async void btnEjecutarPAM_Click(object sender, EventArgs e)
+        {
+            btnEjecutarPAM.Enabled = false;
+            pbPAM.Value = 0;
+            lblResultadoPAM.Text = "Resultado: Ejecutando...";
+
+            for (int i = 0; i <= 100; i += 5)
+            {
+                pbPAM.Value = i;
+                await System.Threading.Tasks.Task.Delay(80);
+            }
+
+            string[] resultadosPosibles =
+            {
+                "Mutación estable detectada",
+                "Fallo genético crítico",
+                "Sin cambios significativos",
+                "Mutación beneficiosa aislada",
+                "Secuencia corrupta - reintentar"
+            };
+
+            string resultado = resultadosPosibles[random.Next(resultadosPosibles.Length)];
+            lblResultadoPAM.Text = "Resultado: " + resultado;
+
+            btnEjecutarPAM.Enabled = true;
+        }
+    }
+}
